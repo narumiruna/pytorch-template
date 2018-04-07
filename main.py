7dd2c71cf50cf877c3ba9470fe59074717b6a665
なるみ <weaper@gmail.com>
2018-04-07 19:53:51 +0800

    add lr_scheduler

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--root', type=str, default='data')
     parser.add_argument('--batch-size', type=int, default=128)
-    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--epochs', type=int, default=40)
     args = parser.parse_args()
 
     args.cuda = torch.cuda.is_available() and not args.no_cuda
@@ -25,12 +25,15 @@ def main():
         model.cuda()
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
 
     train_loader, valid_loader = mnist_loader(args.root, args.batch_size)
 
     trainer = Trainer(model, optimizer, train_loader, valid_loader)
 
     for epoch in range(args.epochs):
+        scheduler.step()
+
         train_loss, train_acc = trainer.train(epoch)
         valid_loss, valid_acc = trainer.validate()
 
