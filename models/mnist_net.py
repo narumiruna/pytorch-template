c60aac4e4e9b5633e53befa869e4cf19674527de
なるみ <weaper@gmail.com>
2018-04-27 14:16:29 +0800

    improve performance

--- models/mnist_net.py
+++ models/mnist_net.py
@@ -6,19 +6,19 @@ class MNISTNet(nn.Module):
         super(MNISTNet, self).__init__()
 
         self.conv = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=5, stride=2, padding=2),
+            nn.Conv2d(1, 32, kernel_size=5, stride=2),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(32),
             nn.Dropout2d(),
 
-            nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2),
+            nn.Conv2d(32, 64, kernel_size=5, stride=2),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(64),
             nn.Dropout2d()
         )
 
         self.fc = nn.Sequential(
-            nn.Linear(64 * 7 * 7, 1024),
+            nn.Linear(64 * 4 * 4, 1024),
             nn.ReLU(inplace=True),
             nn.Dropout(),
 
