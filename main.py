cc01e6d29156553e6720cd92444d048ce31451ee
なるみ <weaper@gmail.com>
2018-07-07 15:33:41 +0800

    refactor

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from datetime import datetime
 import torch
 from torch import nn, optim
 
-from datasets import mnist_loader
-from networks import Net
-from trainers import Trainer
-from utils import load_json, save_json
+import datasets
+import networks
+import trainers
+import utils
 
 
 def main():
@@ -22,9 +22,9 @@ def main():
 
     device = torch.device('cuda' if args.cuda else 'cpu')
 
-    config = load_json(args.config)
+    config = utils.load_json(args.config)
 
-    model = Net()
+    model = networks.Net()
     if args.parallel:
         model = nn.DataParallel(model)
     model.to(device)
@@ -32,15 +32,17 @@ def main():
     optimizer = optim.Adam(model.parameters(), **config['adam'])
     scheduler = optim.lr_scheduler.StepLR(optimizer, **config['steplr'])
 
-    train_loader, valid_loader = mnist_loader(**config['dataset'])
+    train_loader, valid_loader = datasets.mnist_loader(**config['dataset'])
 
-    trainer = Trainer(model, optimizer, train_loader, valid_loader, device)
+    trainer = trainers.Trainer(model, optimizer, train_loader, valid_loader,
+                               device)
 
-    output_dir = os.path.join(config['output_dir'], datetime.now().strftime('%Y%m%d_%H%M%S'))
+    output_dir = os.path.join(config['output_dir'],
+                              datetime.now().strftime('%Y%m%d_%H%M%S'))
     os.makedirs(output_dir, exist_ok=True)
 
     # save config to output dir
-    save_json(config, os.path.join(output_dir, 'config.json'))
+    utils.save_json(config, os.path.join(output_dir, 'config.json'))
 
     for epoch in range(config['epochs']):
         scheduler.step()
@@ -48,11 +50,15 @@ def main():
         train_loss, train_acc = trainer.train()
         valid_loss, valid_acc = trainer.validate()
 
-        print('epoch: {}/{},'.format(epoch + 1, config['epochs']),
-              'train loss: {:.4f}, train acc: {:.2f}%,'.format(train_loss, train_acc * 100),
-              'valid loss: {:.4f}, valid acc: {:.2f}%'.format(valid_loss, valid_acc * 100))
+        print(
+            'epoch: {}/{},'.format(epoch + 1, config['epochs']),
+            'train loss: {:.4f}, train acc: {:.2f}%,'.format(
+                train_loss, train_acc * 100),
+            'valid loss: {:.4f}, valid acc: {:.2f}%'.format(
+                valid_loss, valid_acc * 100))
 
-        torch.save(model.state_dict(), os.path.join(output_dir, 'model_{:04d}.pt'.format(epoch + 1)))
+        f = os.path.join(output_dir, 'model_{:04d}.pt'.format(epoch + 1))
+        torch.save(model.state_dict(), f)
 
 
 if __name__ == '__main__':
