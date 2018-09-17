2cef4bb7c969b92616719d51dbf08a2f4c032c6b
なるみ <weaper@gmail.com>
2018-09-17 23:31:32 +0800

    Refactor

--- main.py
+++ main.py
@@ -30,35 +30,26 @@ def main():
     model.to(device)
 
     optimizer = optim.Adam(model.parameters(), **config['adam'])
-    scheduler = optim.lr_scheduler.StepLR(optimizer, **config['steplr'])
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, **config['steplr'])
 
     train_loader, valid_loader = datasets.mnist_loader(**config['dataset'])
 
-    trainer = trainers.Trainer(model, optimizer, train_loader, valid_loader,
-                               device)
-
     output_dir = os.path.join(config['output_dir'],
                               datetime.now().strftime('%Y%m%d_%H%M%S'))
-    os.makedirs(output_dir, exist_ok=True)
+
+    trainer = trainers.Trainer(
+        model,
+        optimizer,
+        train_loader,
+        valid_loader,
+        device,
+        output_dir,
+        lr_scheduler=lr_scheduler)
 
     # save config to output dir
     utils.save_json(config, os.path.join(output_dir, 'config.json'))
 
-    for epoch in range(config['epochs']):
-        scheduler.step()
-
-        train_loss, train_acc = trainer.train()
-        valid_loss, valid_acc = trainer.validate()
-
-        print(
-            'epoch: {}/{},'.format(epoch + 1, config['epochs']),
-            'train loss: {:.4f}, train acc: {:.2f}%,'.format(
-                train_loss, train_acc * 100),
-            'valid loss: {:.4f}, valid acc: {:.2f}%'.format(
-                valid_loss, valid_acc * 100))
-
-        f = os.path.join(output_dir, 'model_{:04d}.pt'.format(epoch + 1))
-        torch.save(model.state_dict(), f)
+    trainer.fit(config['epochs'])
 
 
 if __name__ == '__main__':
