023144866fd8f3d83f71f67aac97c9634f86c220
なるみ <weaper@gmail.com>
2019-04-16 22:54:57 +0800

    Add timing.py

--- src/utils/timing.py
+++ src/utils/timing.py
@@ -6,17 +6,10 @@ LOGGER = get_logger(__name__)
 
 
 def timeit(func):
-    from ..metrics import Average  # fix circular import
-    time_average = Average()
-
     def timed(*args, **kwargs):
         start = time()
         output = func(*args, **kwargs)
-        end = time()
-        t = end - start
-        time_average.update(t, number=1)
-        LOGGER.info('%s took %.6f seconds, average: %s seconds',
-                    func.__qualname__, t, time_average)
+        LOGGER.info('%s took %.6f seconds.', func.__qualname__, time() - start)
         return output
 
     return timed
