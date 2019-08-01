44030d501fc388f992617f5dbb0710fd518c32de
なるみ <weaper@gmail.com>
2019-08-01 20:23:21 +0800

    Add load_yaml and save_yaml

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
 from .factory import get_factory
 from .log import get_logger
 from .timing import timeit
-from .utils import load_json, save_json
+from .utils import load_json, load_yaml, save_json, save_yaml
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,13 +1,29 @@
 import json
 import os
 
+import yaml
 
 
 def load_json(f):
+    data = None
     with open(f, 'r') as fp:
-        return json.load(fp)
+        data = json.load(fp)
+    return data
 
-def save_json(obj, f, **kwargs):
+
+def save_json(data, f, **kwargs):
     os.makedirs(os.path.dirname(f), exist_ok=True)
     with open(f, 'w') as fp:
-        json.dump(obj, fp, **kwargs)
+        json.dump(data, fp, **kwargs)
+
+
+def load_yaml(f):
+    data = None
+    with open(f, 'r') as fp:
+        data = yaml.safe_load(fp)
+    return data
+
+
+def save_yaml(data, f, **kwargs):
+    with open(f, 'w') as fp:
+        yaml.safe_dump(data, fp, **kwargs)
